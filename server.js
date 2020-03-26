a953d7efb6fcaf6fd572cef72d499c439292c57d
Josué Monteiro <eaglejosue@gmail.com>
2020-03-26 01:15:52 -0300

    Server

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 'use strict'
+
+import http from 'http';
+import express from 'express';
+const debug = require('debug')('nodestr:server');
+
+const app = express();
+const port = 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+const router = express.Router();
+
+const route = router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: "Node API",
+        version: "0.0.1"
+    })
+});
+app.use('/', route);
+
+server.listen(port);
+console.log('API executando na porta ' + port);
